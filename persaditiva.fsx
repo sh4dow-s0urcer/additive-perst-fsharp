46c58dc821bffdbe0133ff9a7e867b08c5a6a4aa
unknown <axelgrullon@outlook.com>
2021-05-07 20:53:03 -0400

    PersAditivaFSharp 4.0

--- persaditiva.fsx
+++ persaditiva.fsx
@@ -1,16 +1,18 @@
 let sumaDigitos n =
-	let rec loop n ady vals =
-		let actual = n % 10I
-		if n > 0I then
-			loop (n/10I) (ady + actual) (actual::vals)
-		else ady, vals
-	let res, sts = loop n 0I []
-	List.map string sts|> String.concat "+"
-	|> fun s -> printfn "%s = %A" s res
-	res
+    let rec loop n ady vals =
+        let actual = n % 10I
+        if n > 0I then
+            loop (n/10I) (ady + actual) (actual::vals)
+        else ady, vals
+    let res, sts = loop n 0I []
+    List.map string sts|> String.concat "+"
+    |> fun s -> printfn "%s = %A" s res
+    res
 
 let persAditiva n =
-	let rec loop n veces =
-		if n < 10I then veces
-		else loop (sumaDigitos n) (veces + 1)
-	loop (System.Numerics.BigInteger.Parse(n.ToString())) 0
+    let rec loop n veces =
+        if n < 10I then veces
+        else loop (sumaDigitos n) (veces + 1)
+    loop (System.Numerics.BigInteger.Parse(n.ToString())) 0
+
+persAditiva 4567891234;
